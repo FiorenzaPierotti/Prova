34d04d6b1b33391b941769c3fb2e120b614e7937
FiorenzaPierotti <65174854+FiorenzaPierotti@users.noreply.github.com>
2020-06-05 17:23:47 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function myFunction (){
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-    if (!event.target.matches('.burger')) {
+    if (!event.target.matches('.burger') && !event.target.matches('.line')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
       /*var dropdown = document.querySelector(".dropdown-content");
       if (dropdown.classList.contains('show')) {
@@ -25,6 +25,76 @@ window.onclick = function(event) {
   }
 
 // Toggle buttons of the menu nav on click
+
+//-------------------------------------------------
+function MenuItem(item) {
+	this.id = item.id || 0;
+  this.link = item.link || '';
+  this.css = item.css || '';
+	this.label = item.label || '';
+  this.active = item.active || false;
+  
+  this.setActive = function (bool) {
+  	this.active = bool
+  }  
+  
+  setHref = () => {
+  	return '#' + this.link
+  }
+  
+  this.createAnchor = function () {
+  	let a = document.createElement('a');
+    a.classList.add(this.css);
+    a.href = setHref();
+    a.innerHTML = this.label;
+    a.addEventListener('click', () => {
+      this.active = !this.active
+    })
+    if(this.active){
+    	a.classList.add('active');
+    }
+    
+    return a;
+  }
+}
+
+const itemHome = {id: 1, css: 'home', link:'home', label:'Home', active: true};
+const itemPort = {id: 2, css: 'port', link:'port', label:'Port', active: false};
+const itemBlog = {id: 3, css: 'blog', link:'blog', label:'Blog', active: false};
+
+let newItemHome = new MenuItem(itemHome).createAnchor();
+
+let newItemPort = new MenuItem(itemPort).createAnchor();
+
+let newItemBlog = new MenuItem(itemBlog).createAnchor();
+
+console.log(newItemHome)
+console.log(newItemPort)
+console.log(newItemBlog)
+
+//-------------------------------------------------------------
+
+function createMenuListItem(){  
+    const li = document.createElement('li');
+    li.appendChild(newItemHome);
+    document.querySelector('#menu').appendChild(li);  
+}
+
+function createBurgerMenuItem(){    
+  document.querySelector('#myDropdown').appendChild(newItemHome); 
+  document.querySelector('#myDropdown').appendChild(newItemPort); 
+  document.querySelector('#myDropdown').appendChild(newItemBlog); 
+}
+
+function createMenuList(){
+  document.querySelector('#menu').innerHTML = ''; // svotato menu
+  document.querySelector('#myDropdown').innerHTML = ''; // svotato menu  
+  createMenuListItem() // sono equivalenti
+  createBurgerMenuItem()
+}
+createMenuList();
+
+/*
 let menuList = [
   {id: 1, css: 'home', link: '#home', label: 'Home', active: true},
   {id: 2, css: 'port', link: '#port', label: 'Port', active: false},
@@ -65,11 +135,11 @@ function createBurgerMenuItem(item){
 
 function menuClick(itemId){
   menuList = menuList.map((item) => {
-    /*if(itemId === item.id){
-      item.active = true;
-    } else {
-      item.active = false;
-    }*/
+    //if(itemId === item.id){
+      //item.active = true;
+    //} else {
+      //item.active = false;
+    //}
     item.active = itemId === item.id
     return item;
   })
@@ -87,6 +157,7 @@ function createMenuList(){
 }
 
 createMenuList();
+*/
 
 // Create Lis in 'port' for the grid
 function createLis(){
